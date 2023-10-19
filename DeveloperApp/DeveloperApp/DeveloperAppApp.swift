dca07db0781af33180b33c29135b52833059b59f
Jose Carlos Rodriguez <jcrodriguez@MacBook-Pro.localdomain>
2023-10-19 16:19:09 +0200

    add proyect and assets

---
+++ DeveloperApp/DeveloperApp/DeveloperAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeveloperAppApp.swift
+//  DeveloperApp
+//
+//  Created by Jose Carlos Rodriguez on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DeveloperAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
